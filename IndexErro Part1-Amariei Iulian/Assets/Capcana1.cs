4c0a71438406a39260634560f8d538124cb08740
iamariei316 <iulian.amariei@student.usv.ro>
2019-12-18 17:21:59 +0200

    Commit2

---
+++ IndexErro Part1-Amariei Iulian/Assets/Capcana1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capcana1 : MonoBehaviour
+{
+    private Vector3 v = new Vector3(0,3f,0);
+    public GameObject capcana;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            capcana.transform.position += v;
+
+    }
+}
---
+++ IndexErro Part1-Amariei Iulian/Assets/Cube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Cube : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+        print("Cube");
+        rb.freezeRotation = true;
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Capcana")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0,8, 0), ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.1f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.1f, 0f, 0f);
+        }
+       // if (Input.GetKey(KeyCode.W))
+       // {
+            transform.Translate(0f, 0f, 0.1f);
+        //}
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0f, 0f, -0.1f);
+        }
+    }
+}
