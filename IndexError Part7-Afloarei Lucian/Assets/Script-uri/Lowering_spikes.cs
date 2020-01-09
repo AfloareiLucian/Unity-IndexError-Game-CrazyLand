240226a222314d9a818c62393f715ff523c62ed0
AfloareiLucian <lucian.afloarei@student.usv.ro>
2020-01-09 23:30:13 +0200

    Modified by Afloarei Lucian

---
+++ IndexError Part7-Afloarei Lucian/Assets/Script-uri/Lowering_spikes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lowering_spikes : MonoBehaviour
+{
+    public GameObject capcana;
+    private bool isActivated = false;
+    public float speedX = 0f, speedY = 0f, speedZ = -0.025f;
+    private int i=0;
+    public int time=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActivated && i<=time)
+        {
+            capcana.transform.Translate(speedX, speedY, speedZ);           
+            i++;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isActivated = true;
+    }
+
+}
---
+++ IndexError Part7-Afloarei Lucian/Assets/Script-uri/Snowman.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowman : MonoBehaviour
+{
+    private bool reverse=false;
+    public float speedX = 0f, speedY = 0f, speedZ = -0.025f;
+    public int timp_atasare = 100;
+    private int i = 0, k = 0;
+    private bool isHooked = false, isDisabled=false;
+    public int time = 0, contor_dezactivare = 0;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (i <= time )
+        {
+            transform.Translate(speedX, speedY, speedZ);
+            i++;
+        }
+        else
+        {
+            transform.Rotate(0, 180, 0);
+            speedY = -speedY;
+            i = 0;
+            reverse = !reverse;
+        }
+
+        if (isHooked == true)
+        {
+            if (k <= timp_atasare)
+            {
+                if (reverse == false)
+                {
+                    player.transform.position = transform.position + new Vector3(0, 0, -1f);
+                }
+                else
+                {
+                    player.transform.position = transform.position + new Vector3(0, 0, 1f);
+                }
+                k++;
+            }
+            else
+            {
+                k = 0;
+                isHooked = false;
+            }
+        }
+
+        if (isDisabled)
+        {
+            if (contor_dezactivare <= 70)
+            {
+                GetComponent<Collider>().enabled = false;
+                contor_dezactivare++;
+            }
+            else
+            {
+                isDisabled = false;
+                contor_dezactivare = 0;
+                GetComponent<Collider>().enabled = true;
+            }
+        }
+
+    }
+
+   private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name.Contains("Cube"))
+        {
+            player = col.gameObject;
+            isHooked = true;
+        }
+    }
+
+}
