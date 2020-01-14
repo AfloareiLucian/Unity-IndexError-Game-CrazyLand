8f5d91e3d647492b1c044b12b5b5426745b6db80
AfloareiLucian <lucian.afloarei@student.usv.ro>
2020-01-14 15:32:32 +0200

    Modified by Afloarei Lucian

---
+++ IndexError Part5-Afloarei Lucian/Assets/Trigger_anim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_anim : MonoBehaviour
+{
+    public GameObject capcana;
+    public string denumire_anim;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Renderer>().enabled = false;
+        anim = capcana.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.Play(denumire_anim);
+
+    }
+
+}
